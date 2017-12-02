3ec3ac180cb11a5d2dc523c0d1a0e7d0475f89bb
Òscar Orti Gonzalez <oscarortigonzalez@Oscars-MacBook-Pro.local>
2017-12-02 21:45:07 +0100

    Create script to encode a videofile, not yet implemented

--- encode_in_pel_domain.m
+++ encode_in_pel_domain.m
@@ -1,9 +1,17 @@
-function [ ] = encode_in_pel_domain( filename )
+function [compensated_image_blocks, compensated_block_positions, mse_block_matrix] = encode_in_pel_domain(filename)
 % ENCODE_IN_PEL_DOMAIN 
 
 video = load_video(filename);
 % video_frames = get_frames(video, 1, Inf);
-video_frames = get_frames(video, 1, 20);
+video_frames = get_frames(video, 1, 4);
+compensated_block_positions = cell(1,1,1);
+
+for i=2:4
+    
+    [compensated_image_blocks, compensated_block_positions(:,:,i), mse_block_matrix] = ... 
+     generate_compensated_image(video_frames(:,:,:,i-1), video_frames(:,:,:,i), 10);
+
+end
 
 end
 
